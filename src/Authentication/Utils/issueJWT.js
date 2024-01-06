a3a5439d471ecc7eafcab7accd82c3c6a95fa5f2
LeTorky <24Torky@gmail.com>
2024-01-06 20:56:28 +0200

    -Created issueJwt util.

---
+++ src/Authentication/Utils/issueJWT.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import fs from "fs";
+
+dotenv.config();
+
+const privateKey = fs.readFileSync("../privatekey.pem", "utf8");
+
+const issueJWT = (payLoad) => {
+  const token = jwt.sign(payLoad, privateKey, { algorithm: "RS256" });
+  return token;
+};
+
+export default issueJWT;
